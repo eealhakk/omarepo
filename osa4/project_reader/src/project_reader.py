f141173828431026c400385cab5fdacb48869624
Eeli Häkkinen <eeli.hakkinen00@gmail.com>
2025-11-11 08:39:09 +0200

    8 teht

--- osa4/project_reader/src/project_reader.py
+++ osa4/project_reader/src/project_reader.py
@@ -1,4 +1,5 @@
 from urllib import request
+import toml
 from project import Project
 
 
@@ -7,9 +8,26 @@ class ProjectReader:
         self._url = url
 
     def get_project(self):
+        print("get projekt alkaa")
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
         print(content)
 
+        data = toml.loads(content)
+        #print("\nTOML muutettuna Python dataksi:")
+        #print(data)
+
+        tool = data["tool"]["poetry"]
+
+        name = tool.get("name")
+        description = tool.get("description")
+
+        dependencies = list(tool.get("dependencies", {}).keys())
+        dev_dependencies = list(tool.get("group", {}).get("dev", {}).get("dependencies", {}).keys())
+
+
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        #return Project("Test name", "Test description", [], [])
+        #print("Project")
+        #print(Project(name, description, dependencies, dev_dependencies))
+        return Project(name, description, dependencies, dev_dependencies)
